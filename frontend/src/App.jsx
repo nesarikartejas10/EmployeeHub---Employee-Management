d8b85184aa921c1f9654e955049f6179395762f5
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-01-31 21:24:21 +0530

    add routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import RootLayout from "./layout/RootLayout";
+import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
+
 function App() {
-  return <>Hello</>;
+  return (
+    <Routes>
+      <Route element={<RootLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
