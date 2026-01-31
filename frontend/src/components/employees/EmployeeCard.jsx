e00d6d7e9d6b707cf8436b8e11d7973a38fb06d7
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-01-31 21:25:38 +0530

    created employee and employee card component

---
+++ frontend/src/components/employees/EmployeeCard.jsx
@@ -0,0 +1,40 @@
+import {
+  MdOutlineFavoriteBorder,
+  MdOutlineModeEdit,
+  MdOutlineDeleteOutline,
+} from "react-icons/md";
+
+const EmployeeCard = () => {
+  return (
+    <li className="list-row">
+      <div>
+        <img
+          className="size-10 rounded-box"
+          src="https://img.daisyui.com/images/profile/demo/1@94.webp"
+        />
+      </div>
+      <div>
+        <div>Dio Lupa</div>
+        <div className="text-xs uppercase font-semibold opacity-60">
+          Remaining Reason
+        </div>
+      </div>
+      <p className="list-col-wrap text-xs">
+        "Remaining Reason" became an instant hit, praised for its haunting sound
+        and emotional depth. A viral performance brought it widespread
+        recognition, making it one of Dio Lupa’s most iconic tracks.
+      </p>
+      <button className="btn btn-square btn-ghost">
+        <MdOutlineModeEdit size={20} />
+      </button>
+      <button className="btn btn-square btn-ghost">
+        <MdOutlineDeleteOutline size={20} />
+      </button>
+      <button className="btn btn-square btn-ghost">
+        <MdOutlineFavoriteBorder size={20} />
+      </button>
+    </li>
+  );
+};
+
+export default EmployeeCard;
---
+++ frontend/src/components/employees/Employees.jsx
@@ -0,0 +1,25 @@
+import EmployeeCard from "./EmployeeCard";
+
+const Employees = () => {
+  return (
+    <section className="max-w-6xl w-full m-auto">
+      <ul className="list">
+        <EmployeeCard />
+        <EmployeeCard />
+        <EmployeeCard />
+        <EmployeeCard />
+        <EmployeeCard />
+        <EmployeeCard />
+        <EmployeeCard />
+        <EmployeeCard />
+        <EmployeeCard />
+        <EmployeeCard />
+        <EmployeeCard />
+        <EmployeeCard />
+        <EmployeeCard />
+      </ul>
+    </section>
+  );
+};
+
+export default Employees;
